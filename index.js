656ed5e3bf7b6d9bb9255e7cc1ce134c6999e41a
Vivek Dhiman <59130053+vivrehal@users.noreply.github.com>
2024-03-18 07:05:19 +0000

    final commit

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.post('/bfhl',(req,res)=>{
+    const {data}=req.body;
+    const even=data.filter((num)=>typeof num=='number' && num%2==0);
+    const odd=data.filter((num)=>typeof num=='number' && num%2==1);
+    const alpha=data.filter((num)=>typeof num=='string');
+    const upper=alpha.map((al)=>al.toUpperCase());
+    res.json({
+        is_success: true,
+        user_id: "vivek_dhiman_01062003",
+        email : "vivek1552.be21@chitkara.edu.in",
+        roll_number:2110991552,
+        odd_numbers: odd,
+        even_numbers: even,
+        alphabets: upper
+    }).status(200);
+
+
+})
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
